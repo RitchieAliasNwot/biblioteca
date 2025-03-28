14422c650ff3682ee845186ec55be04c721e1952
mssua <mssuaa5@gmail.com>
2025-03-28 21:41:34 +0100

    Merge Ashly

---
+++ src/biblioteca/Ashly.java
@@ -0,0 +1,8 @@
+package biblioteca;
+
+public class Ashly {
+
+	public static void main(String[] args) {
+		System.out.println("Ashly");
+	}
+}
--- src/biblioteca/HelloWorld.java
+++ src/biblioteca/HelloWorld.java
@@ -2,8 +2,6 @@ package biblioteca;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-    System.out.println("Este texto es de Ricardo");
 		System.out.println("Este texto es de Ash");
 	}
 }
