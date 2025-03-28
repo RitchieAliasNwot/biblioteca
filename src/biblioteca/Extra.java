89b200a004d0b552da226375109bd9d04f3e583d
RitchieAliasNwot <richielsincuenta@gmail.com>
2025-03-28 21:31:35 +0100

    Prueba Arch

---
+++ src/biblioteca/Extra.java
@@ -0,0 +1,9 @@
+package biblioteca;
+
+public class Extra {
+
+	public static void main(String[] args) {
+    System.out.println("Este es otro texto de prueba");
+	}
+
+}
--- src/biblioteca/HelloWorld.java
+++ src/biblioteca/HelloWorld.java
@@ -1,8 +1,8 @@
 package biblioteca;
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+    System.out.println("Este texto es de Ricardo");
 	}
 }
